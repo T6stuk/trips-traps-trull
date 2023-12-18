708ae5158a2bdd4749c117c6156d85eae7e9dc12
T6stuk <94644396+T6stuk@users.noreply.github.com>
2023-12-18 12:31:39 +0200

    uuendus

--- src/App.1.js
+++ src/App.1.js
@@ -1,4 +1,6 @@
-export function App() {
+import React from 'react';
+
+export function App1({ averageIncome, setAverageIncome, calculateAllowance, dailyAllowance, sickLeave, setSickLeave, calculateSickLeaveAllowance, sickLeaveAllowance, isChecked, setIsChecked, employerDays, insuranceDays, employerAnswer, insuranceAnswer, compensationTotalDays, totalAnswer, calculateTotal, calculateAll }) {
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -21,13 +23,13 @@ export function App() {
       <div className="surround">
 
         <div className="background1 item1">{/*1*/}
-          <h1>Kalevipoeg</h1>
-          <p>"Kalevipoeg" on 19. sajandil peamiselt Friedrich Reinhold Kreutzwaldi koostatud eesti rahvuseepos eesti rahvaluule ainetel.
+          <h1 className="pealkiri">Qalevipoeq</h1>
+          <p>"Qalevipoeg" on 19. sajandil peamiselt Friedrich Reinhold Kreutzwaldi koostatud eesti rahvuseepos eesti rahvaluule ainetel.
 
            <mark className="highlight">"Kalevipoega"</mark>peetakse üheks eesti kirjanduse olulisemaks teoseks ning selle motiive on hiljem kasutanud mitmed kunstnikud, heliloojad ja kirjanikud. Samuti oli teosel roll eesti rahvusliku eneseteadvuse väljakujunemisel.</p>
         </div>
         <div className="background2 item2">
-          <h2>Õpetatud Eesti Selts ja Faehlmann</h2>
+          <h2 className="pealkiri">Õpetatud Eesti Selts ja Faehlmann</h2>
           <p>19. sajandi alguse rahvusliku liikumise tõusulainel leidsid rahvavalgustajad, et Eesti maarahva rahvuslikku eneseteadvust aitaks turgutada teiste rahvastega võrdväärne kangelaseepos.</p>
           <ul>
            <li>Coffee</li>
@@ -43,9 +45,82 @@ export function App() {
           </ul>
         </div>{/*2*/}
         <div className="background3 item3 image-little">
-        <img className="image-little" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/A%29_Lamav_akt_b%29_Kalev_kotka_se%2C_Oskar_Kallis%2C_EKM_j_24456-ab_M_5624-ab.jpg/440px-A%29_Lamav_akt_b%29_Kalev_kotka_se%2C_Oskar_Kallis%2C_EKM_j_24456-ab_M_5624-ab.jpg" alt="Italian Trulli" ></img>
+        <img className="image-little" src="https://i.ibb.co/bBnvxWb/pilt.png" alt="Italian Trulli" ></img> {/*Aknaga pilt*/}
         </div>{/*3*/}
-        <div className="background4 item4 image-big">
+        
+
+       
+        <div className="item4 image-big">{/*Kalkulaatroi tabloo*/}
+         <h3>Compensation<br />
+         Calculator</h3>
+         <div>{/*input*/}
+         <form>
+          <div className="input-container">
+           <label>Average Income</label>
+           <div className="input-container-border">
+            <input typeName="number" value={averageIncome} onChange={e => setAverageIncome(e.target.value)}></input>
+            <span className="static-text">€</span>
+           </div>
+          </div>
+
+          <div className="input-container">
+           <label>Days on sick-leave</label>
+           <div className="input-container-border">
+            <input typeName="number" value={sickLeave} onChange={e => setSickLeave(e.target.value)} ></input>
+            <span className="static-text">days</span>
+           </div>
+          </div>
+          
+          <div className="checkbox-container">
+           <input type="checkbox" id="myCheckbox" checked={isChecked} onChange={e => setIsChecked(e.target.checked)} ></input>
+           <label for="myCheckbox" class="checkbox-border"></label>
+           <span className="label-text">I have tuberculosis</span>
+          </div>
+
+         </form>
+         </div>
+         
+         <div>
+         <button className="button" onClick={calculateAll}>Calculate</button>
+         </div>
+
+         <div className="joon"></div>
+
+         <div className="cover">
+          <div className="cover-column">
+           <div className="cover-column-gap">
+            <div className="cover-column-text1">The employer compensates</div>
+            <div className="cover-column-text2">{employerDays} days</div>
+           </div>
+           <div>
+            <div className="cover-column-text3">{employerAnswer}€</div>
+            <div className="cover-column-text4">Daily allowance: {dailyAllowance.toFixed(2)} €</div>
+           </div>
+          </div>
+
+          <div className="cover-column">
+           <div className="cover-column-gap">
+            <div className="cover-column-text1">Health Insurance compensates</div>
+            <div className="cover-column-text2">{insuranceDays} days</div>
+           </div>
+           <div>
+            <div className="cover-column-text3">{insuranceAnswer}€</div>
+            <div className="cover-column-text4">Daily allowance {sickLeaveAllowance.toFixed(2)}  €</div>
+           </div>
+          </div>
+         </div>
+
+         <div className="joon"></div>
+
+         <div className="solution">
+          <div className="solution-text">Compensation total for {compensationTotalDays} days (net)</div> 
+          
+          <div className="solution-text-awnser">{totalAnswer}€</div>
+         </div>
+          
+
+        
+
         </div>{/*4*/}
         <div className="background5 item5">
         </div>{/*5*/}
@@ -55,3 +130,5 @@ export function App() {
     </div>
   );
 }
+
+export default App1; 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,124 @@
-// eslint-disable-next-line no-unused-vars
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import { App } from './App.1';
+import App1 from './App.1';
+
+function App() {
+    const [averageIncome, setAverageIncome] = useState(1500);
+    const [dailyAllowance, setDailyAllowance] = useState(0);
+
+    const [sickLeave, setSickLeave] = useState(7);
+    const [sickLeaveAllowance, setSickLeaveAllowance] = useState(0);
+    const [isChecked, setIsChecked] = useState(false);
+
+    const [employerDays, setEmployerDays] = useState(0);
+
+    const [insuranceDays, setInsuranceDays] = useState(0);
+
+    const [employerAnswer, setEmployerAnswer] = useState(0);
+
+    const [insuranceAnswer, setInsuranceAnswer] = useState(0);
+
+    const [compensationTotalDays, setCompensationTotalDays] = useState(0);
+
+    const [totalAnswer, setTotalAnswer] = useState(0);
+
+
+    const calculateAllowance = () => {
+        let allowance;
+        if (averageIncome >= 1500) {
+            allowance = 28.00 + (averageIncome - 1500) * 0.01; // Increase allowance by 1% of the amount over 1500
+        } else {
+            allowance = averageIncome * 0.01867; // Adjust this calculation as needed
+        }
+        setDailyAllowance(allowance);
+
+        if (!isChecked) {
+            setSickLeaveAllowance(0);
+        }
+    }
+
+
+    const calculateEmployerDays = () => {
+        const days = Math.round(sickLeave * 0.57);
+        setEmployerDays(days);
+    };
+    
+    const calculateEmployerAnswer = () => {
+        const answer = dailyAllowance * employerDays;
+        setEmployerAnswer(answer);
+    };
+    
+    const calculateInsuranceAnswer = () => {
+        const answer2 = sickLeaveAllowance * insuranceDays;
+        setInsuranceAnswer(answer2);
+    };
+    
+    const calculateCompensationTotalDays = () => {
+        setCompensationTotalDays(sickLeave);
+    };
+
+
+
+    const calculateTotal = () => {
+        const total = (insuranceAnswer) + (employerAnswer);
+        setTotalAnswer(total);
+    };
+
+
+    const calculateSickLeaveAllowance = () => {
+        if (isChecked) {
+            let allowance2;
+            if (sickLeave >= 7) {
+                allowance2 = 28.00 + (sickLeave - 7) * 0.01; // Increase allowance by 1% of the amount over 1500
+            } else {
+                allowance2 = sickLeave * 0.01867; // Adjust this calculation as needed
+            }
+            setSickLeaveAllowance(allowance2);
+        }
+
+    };
+
+    const calculateInsuranceDays = () => {
+        const days = isChecked ? Math.round(sickLeave * 0.57) : 0;
+        setInsuranceDays(days);
+    };
+
+    const calculateAll = () => {
+        calculateEmployerDays();
+        calculateAllowance();
+        calculateInsuranceDays();
+        calculateSickLeaveAllowance();
+        calculateCompensationTotalDays();
+    
+        calculateEmployerAnswer();
+        calculateInsuranceAnswer();
+    
+        calculateTotal();
+    };
+
+    return (
+        <App1 
+            averageIncome={averageIncome} 
+            setAverageIncome={setAverageIncome} 
+            calculateAllowance={calculateAllowance} 
+            dailyAllowance={dailyAllowance}
+            sickLeave={sickLeave}
+            setSickLeave={setSickLeave} 
+            calculateSickLeaveAllowance={calculateSickLeaveAllowance} 
+            sickLeaveAllowance={sickLeaveAllowance}
+            isChecked={isChecked}
+            setIsChecked={setIsChecked}
+            employerDays={employerDays}
+            insuranceDays={insuranceDays}
+            employerAnswer={employerAnswer}
+            insuranceAnswer={insuranceAnswer}
+            compensationTotalDays={compensationTotalDays}
+            totalAnswer={totalAnswer}
+            calculateTotal={calculateTotal}
+            calculateAll={calculateAll}
+        />
+
+    );
+}
 
 export default App;
